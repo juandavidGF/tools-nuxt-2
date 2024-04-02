85d74d2fa0dc814f1251ab5502cca41bcfa92714
juandavidGF <davad701@gmail.com>
2024-04-02 17:49:35 -0500

    add count for test

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1 class="text-3xl font-bold text-red-300">
-    Hello world!
-  </h1>
+  <NuxtLayout>
+    <div class="my-3">
+      <h1 class="text-3xl font-bold text-red-300">
+        Hello world!
+      </h1>
+      <button class="btn btn-outline">Start</button>
+      <button class="btn btn-outline" 
+        @click="count++">{{ count }}
+      </button>
+    </div>
+  </NuxtLayout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="m-auto max-w-2xl min-h-screen">
+    <slot />
+  </div>
+</template>
