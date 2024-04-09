85ecf7608cd47154da7528f69d6271bd9992a9df
juandavidGF <davad701@gmail.com>
2024-04-09 14:59:46 -0500

    add basic layout

--- app.vue
+++ app.vue
@@ -1,13 +1,14 @@
 <template>
   <NuxtLayout>
-    <div class="my-3">
-      <h1 class="text-3xl font-bold text-red-300">
-        Hello world!
-      </h1>
-      <button class="btn btn-outline">Start</button>
-      <button class="btn btn-outline" 
-        @click="count++">{{ count }}
-      </button>
+    <div class="mx-auto w-[500px]">
+      <div class="relative mx-auto my-3 w-[400px] h-[400px] border border-black rounded-md" 
+      :class="{'cursor-pointer': !imageUrl}"
+      @click="handleNewImg">
+        <input type="file" ref="fileUploaded" @change="handleImgUploaded" hidden>
+        <img v-if="imageUrl" :src="imageUrl" class="object-contain m-auto h-full" />
+        <button v-if="imageUrl" @click.stop="removeImage" class="absolute top-1 right-2">X</button>
+      </div>
+      <button class="btn btn-neutral m-auto flex">Remove Background</button>
     </div>
   </NuxtLayout>
 </template>
@@ -16,8 +17,21 @@
 export default {
   data() {
     return {
-      count: 0,
+      imageUrl: null,
     }
   },
+  methods: {
+    handleNewImg() {
+      this.$refs.fileUploaded.click();
+    },
+    handleImgUploaded(event) {
+      const image = event.target.files[0];
+      this.imageUrl = URL.createObjectURL(image);
+    },
+    removeImage() {
+      URL.revokeObjectURL(this.imageUrl);
+      this.imageUrl = null;
+    }
+  }
 }
 </script>
