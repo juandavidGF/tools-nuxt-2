48192cf13802da3721654d78d0edcd00bbaac8f4
juandavidGF <davad701@gmail.com>
2024-04-09 20:44:20 -0500

    remove-bg test data revived

--- api/remove-bg.js
+++ api/remove-bg.js
@@ -1,8 +1,21 @@
-export default function handler(req, res) {
-  const { body, method } = req;
+const axios = require('axios');
+const FormData = require('form-data');
+import formidable from 'formidable';
 
-  if(method !== 'POST') {
+export default function handler(req, res) {
+  if(req.method !== 'POST') {
     return res.status(404).json({ error: 'bad request, just POST allowed'});
   }
-  res.status(200).json({ name: 'John Doe' })
+
+  const form = new formidable.IncomingForm();
+
+  form.parse(rqu, (err, fields, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error parsing the form data.' });
+    }
+    console.log(fields);
+    console.log(files);
+  })
+
+  res.status(200).json({ name: 'John Doe' });
 }
