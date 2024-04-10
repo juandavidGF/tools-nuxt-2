41401dd3a1a5e63cdf28adbcb8339408451388d1
juandavidGF <davad701@gmail.com>
2024-04-09 20:23:22 -0500

    test post

--- api/hello2.js
+++ api/hello2.js
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
+  const { body, method } = req;
+
+  if(method !== 'POST') {
+    return res.status(404).json({ error: 'bad request, just POST allowed'});
+  }
   res.status(200).json({ name: 'John Doe' })
 }
